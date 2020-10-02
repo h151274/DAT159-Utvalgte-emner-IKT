c69cb81931d0039fb2877afdee107b5e38d3bd39
h151274 <bonn_96aa@hotmail.com>
2020-10-02 14:58:38 +0200

    crypto oblig setup

--- refactoring/dat159/src/PullUp/Main.java
+++ refactoring/dat159/src/PullUp/Main.java
@@ -1,7 +1,15 @@
+package PullUp;
 
-public class task3 {
-	public static void main (String args[]) { 
-		
+public class Main {
+
+	public static void printResults(SuperDuper a, Super b) {
+		System.out.println("Called from SuperDuper class: " + a.function() + "\n" + "Called from Super class: "
+				+ b.function() + "\n" + "get from SuperDuper: " + b.test());
 	}
 
+	public static void main(String[] args) {
+		SuperDuper A = new SuperDuper();
+		Super B = new Super();
+		printResults(A, B);
+	}
 }
